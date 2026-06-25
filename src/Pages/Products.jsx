de2d646cf6acea3a9bf4a43ab8264ef4da3ebb88
Masthan <masthan.eedara@gmail.com>
2026-06-25 13:08:36 +0530

    latest dairy app updatev2.4.3

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -271,7 +271,7 @@ export default function Products() {
             TABLET = 2 columns
             DESKTOP = 3 columns
           */}
-         <div className="grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-5">
+         <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-5">
             {Array.isArray(products) &&
               products.map((product, index) => (
                 <div
